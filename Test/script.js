cc5ee23b09cf618a6b3fb6b823ebc0c0e172316f
Shiva TEClab <sreddy@tecofva.com>
2021-11-30 17:02:45 -0500

    empty commit

--- Test/script.js
+++ Test/script.js
@@ -1,4 +1,10 @@
 
 
+const myItem = document.querySelector('h2');
 
+myItem.className = 'small'
 
+setInterval(function () {
+        myItem.classList.toggle('big');
+        myItem.classList.toggle('small');
+    },1000)
