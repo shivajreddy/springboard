f1100c54a64ddb959ae6a4a90bee9b23ee25d3da
Shiva Reddy <shivajayaramireddy@gmail.com>
2021-12-02 18:17:46 -0500

    empty

---
+++ 4.5-MemoryGame/script.js
@@ -0,0 +1,24 @@
+
+const result = document.querySelector('#result');
+
+result.innerText = "shiva"
+result.innerText = "reddy"
+
+
+const button = document.querySelector('#push');
+
+let count = 0
+
+function addText() {
+    // let someNumber = Math.floor((Math.random()) * 20)
+    count = count + 1
+    result.innerText = `${count}`
+    // return count
+}
+
+
+
+
+
+
+
--- Test/script.js
+++ Test/script.js
@@ -1,10 +1,6 @@
 
 
-const myItem = document.querySelector('h2');
 
-myItem.className = 'small'
 
-setInterval(function () {
-        myItem.classList.toggle('big');
-        myItem.classList.toggle('small');
-    },1000)
+
+
