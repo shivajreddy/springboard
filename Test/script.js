7349c0501fd69b2cfb56de1f87be399964517961
Shiva Reddy <shivajayaramireddy@gmail.com>
2021-12-03 09:27:59 -0500

    update2

--- Test/script.js
+++ Test/script.js
@@ -1,6 +1,28 @@
 
+const ul = document.querySelector('ul')
+
+
+const li = document.querySelector('li');
+
+
+ul.addEventListener('click', function(event){
+    let thing = event.target
+    console.log(thing.dataset)
+    console.log(thing.getAttribute('data-id'))
+    // console.log(thing.dataset.id)
+})
+
+const preferences = {
+    fontSize: '18px',
+    favColor: 'purple'
+}
+
+
+localStorage.setItem('preferences', preferences);
 
 
 
-
+JSON
+JSON.stringify
+JSON.
 
