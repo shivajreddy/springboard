79740a1f054de51289a3c086eef323af7d400a00
Shiva Reddy <shivajayaramireddy@gmail.com>
2021-12-26 14:24:49 -0500

    finished 8.2

---
+++ 08-AdvancedArrayMethods/08.2-some,very,find,findIndex/find-findindex.js
@@ -0,0 +1,53 @@
+/* 
+Write a function called `findUserByUsername` which accepts an array of objects, each with a key of username, and a string. The function should return the first object with the key of username that matches the string passed to the function. If the object is not found, return undefined. 
+
+const users = [
+  {username: 'mlewis'},
+  {username: 'akagen'},
+  {username: 'msmith'}
+];
+
+findUserByUsername(users, 'mlewis') // {username: 'mlewis'}
+findUserByUsername(users, 'taco') // undefined
+*/
+function findUserByUsername(usersArray, username) {
+    return usersArray.find((obj=>obj.username == username))
+}
+
+/*
+Write a function called `removeUser` which accepts an array of objects, each with a key of username, and a string. The function should remove the object from the array. If the object is not found, return undefined. 
+
+const users = [
+  {username: 'mlewis'},
+  {username: 'akagen'},
+  {username: 'msmith'}
+];
+
+removeUser(users, 'akagen') // {username: 'akagen'}
+removeUser(users, 'akagen') // undefined
+*/
+function removeUser(usersArray, username) {
+    const idx = (usersArray.findIndex((i)=> i.username === username))
+    if (idx != -1)
+    {
+        const removed = usersArray.splice(idx,1)
+        return (removed[0])
+    }
+
+}
+
+//? My own Find function for an array
+const nums = [1,2,3,4,5]
+
+function myFind(arr, f) {
+
+    for(i of arr)
+    {
+        if ( f(i) === true)
+        {
+            return i
+        }
+    }
+}
+
+// console.log(myFind(nums,(i)=>i>3))
