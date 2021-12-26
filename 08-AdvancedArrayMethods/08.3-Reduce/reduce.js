15c8fe9e601ad2cc0877023cb64ea2e6f31091e8
Shiva Reddy <shivajayaramireddy@gmail.com>
2021-12-26 16:31:01 -0500

    finished 8.3

--- 08-AdvancedArrayMethods/08.3-Reduce/reduce.js
+++ 08-AdvancedArrayMethods/08.3-Reduce/reduce.js
@@ -6,7 +6,10 @@ Examples:
     extractValue(arr,'name') // ['Elie', 'Tim', 'Matt', 'Colt']
 */
 
-function extractValue(arr, key) {}
+function extractValue(arr, key) {
+    return arr.map((i)=>i[key])
+}
+
 
 /*
 Write a function called vowelCount which accepts a string and returns an object with the keys as the vowel and the values as the number of times the vowel appears in the string. This function should be case insensitive so a lowercase letter and uppercase letter should count
@@ -19,7 +22,20 @@ Examples:
     vowelCount('I Am awesome and so are you') // {i: 1, a: 4, e: 3, o: 3, u: 1};
 */
 
-function vowelCount(str) {}
+function vowelCount(str) {
+    const result = {};
+    str = str.toLowerCase();
+
+    for (char of str){
+        if(char === 'a' || char === 'e' || char === 'i' || char === 'o' || char === 'u'){
+            if(result[char]) result[char] += 1
+            else{result[char] = 1}
+        }
+    }
+
+    return result
+}
+
 
 /*
 Write a function called addKeyAndValue which accepts an array of objects and returns the array of objects passed to it with each object now including the key and value passed to the function.
@@ -36,7 +52,13 @@ Examples:
        ]
 */
 
-function addKeyAndValue(arr, key, value) {}
+function addKeyAndValue(arr, key, value) {
+    return arr.map((obj)=>{
+        obj[key] = value
+        return obj
+    })
+}
+
 
 /*
 Write a function called partition which accepts an array and a callback and returns an array with two arrays inside of it. The partition function should run the callback function on each value in the array and if the result of the callback function at that specific value is true, the value should be placed in the first subarray. If the result of the callback function at that specific value is false, the value should be placed in the second subarray. 
@@ -60,4 +82,23 @@ Examples:
     partition(names, isLongerThanThreeCharacters) // [['Elie', 'Colt', 'Matt'], ['Tim']]
 */
 
-function partition(arr, callback) {}
+function isLongerThanThreeCharacters(str){
+    if (str.length > 3)
+    {
+        return true
+    }
+    else{ return false}
+}
+
+function partition(arr, callback) {
+    const result = [[], []]
+    for(i of arr)
+    {
+        if (callback(i)) result[0].push(i)
+        else{result[1].push(i)}
+    }
+    return result
+}
+
+const names = ['Elie', 'Colt', 'Tim', 'Matt'];
+console.log(partition(names, isLongerThanThreeCharacters)) // [['Elie', 'Colt', 'Matt'], ['Tim']]
