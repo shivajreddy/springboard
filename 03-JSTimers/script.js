19c63512e4dccfba6843499302ce46d58b43c2bf
Shiva Reddy <shivajayaramireddy@gmail.com>
2021-12-28 19:08:53 -0500

    finished 3-js_timers

--- 03-JSTimers/script.js
+++ 03-JSTimers/script.js
@@ -1,22 +1,43 @@
+//* CountDown Function
+const countDown = (num) => {
 
-const countdown = (num) =>{
-    // for(let i =0; i<num ; i++){
-    for (let i = 0; i < num ; i++)
-    {
-        setTimeout(console.log(i), 1000)
+    let n = num
+
+    const print = (n)=>{
+        if(n > 0) console.log(n);
+        if(n == 0) console.log('Done!');
     }
-}
 
-// console.log(countdown(4))
-
-const f = () => console.log('hello there');
-
-let num = 5;
-
-for(let i=0; i<num; i++){
-
-    // setInterval(f, 1000);
-
-    setTimeout(f,1000)
+    setInterval(()=>{
+        print(n)
+        n -= 1
+    },1000);
 
 }
+
+// countDown(12);
+
+
+//*RandomGame Function
+
+const randomGame = () => {
+
+    const randomNumber = ()=>Math.random()
+    let number = 0;
+    let counter = 0;
+
+    const id = setInterval(()=>{
+        if (number < 0.75) 
+        {
+            counter += 1;
+            number = randomNumber()
+            console.log(number)
+            
+        }
+        else {
+            console.log(`It took ${counter} times to reach ${number} which is > 0.75`);
+            clearInterval(id)
+        }
+    }, 1000);
+}
+randomGame()
