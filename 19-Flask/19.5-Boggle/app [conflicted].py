60a5f10ef29cca53e1c9cb3e5a25f957f377a2ca
Shiva Reddy <shivajayaramireddy@gmail.com>
2022-02-07 16:58:49 -0500

    bigo finished

--- 19-Flask/19.5-Boggle/app [conflicted].py
+++ 19-Flask/19.5-Boggle/app [conflicted].py
@@ -15,17 +15,15 @@ toolbar = DebugToolbarExtension(app)
 def home_page():
     if session['boggle-board']:
         boggle_board = session['boggle-board']
-        # user_guess = session['user-guess']
     else:
-        session.clear()
         session['boggle-board'] = boggle_game.make_board()
     
     return render_template('index.html', board=boggle_board)
 
 
-@app.route('/get-word', methods=["POST"])
+@app.route('/get-word/<string:guess>')
 def get_word():
     user_word = request.form['guess']
     session['user-guess'] = user_word
-    flash(user_word)
+    
     return redirect('/')
--- 19-Flask/19.5-Boggle/static/script.js
+++ 19-Flask/19.5-Boggle/static/script.js
@@ -1,2 +1,9 @@
 console.log("Hi Mom!");
 
+const button = document.querySelector('button');
+
+button.addEventListener('click', function doSomething(){
+    const user_input = document.querySelector('input').value;
+    console.log(user_input);
+});
+
