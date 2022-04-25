96b1ee111fcc840461c53ac3c274a76ee43814fa
Shiva Reddy <shivajayaramireddy@gmail.com>
2022-04-25 07:58:52 -0400

    .

--- 31-Node/31.4-Node/followalong/guestlist.js
+++ 31-Node/31.4-Node/followalong/guestlist.js
@@ -1,18 +1,25 @@
 const faker = require('faker');
 const axios = require('axios');
+// const helper = require('helper');
+const helper = require('./helper');
 
 // console.log(faker.name.findName());
 // console.log(process.env);
 // console.log(process);
 
-process.on('exit', ()=>{
-  console.log('this exited');
-})
+// process.on('exit', ()=>{
+//   console.log('this exited');
+// })
 
-setInterval(() => {
-  console.log('hellooooo');
-}, 1000);
+// setInterval(() => {
+//   console.log('hellooooo');
+// }, 1000);
 
-setInterval(() => {
-  process.exit(2)
-}, 6000);
+// setInterval(() => {
+//   process.exit(2)
+// }, 6000);
+
+
+console.log(helper.add(10,20));
+console.log(helper.sub(20,10));
+console.log(helper.mult(10,20));
---
+++ 31-Node/31.4-Node/followalong/helper.js
@@ -0,0 +1,12 @@
+const add = (a,b) => a+b;
+
+const sub = (a,b) => a-b;
+
+const mult = (a,b) => a*b;
+
+
+module.exports = {
+  add : add,
+  sub: sub,
+  mult: mult
+};
