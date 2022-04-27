e1e5a207bf9aed9d60812f77a4447e1fa7a19041
shivajreddy <shivajayaramireddy@gmail.com>
2022-04-27 07:57:57 -0400

    31.2 part1 done

--- 31-Node/31.2-Promises/Exercise/part1.js
+++ 31-Node/31.2-Promises/Exercise/part1.js
@@ -1,50 +1,28 @@
-const axios = require('axios');
 const baseURL = 'http://numbersapi.com'
 const favNum = 2
-
-// const makeRequest = $.get(`${baseURL}/${favNum}`)
+const $numberFactsDiv = $('.number-facts')
+const generateButton = $('button')
 
 // by making promise object
 function makeRequest(){
   return new Promise((resolve, reject) => {
-    // const response = $.get(`${baseURL}/${favNum}`)
-    const reqponse = axios.get(`${baseURL}/${favNum}`)
-    console.log(response)
+    const response = axios.get(`${baseURL}/${favNum}`)
     resolve(response)
   })
 }
 
-makeRequest().then((response)=>{
-  console.log(`Successful request`)
-  console.log(response)
-})
-
-
-// by using axios to send get request
-
-const totalFacts = (times) => {
-  for (let i=0; i<times; i++){
-
-    $.get(`${baseURL}/${favNum}?json`).then(
-      data => {console.log(data);}
-    )
-    
-  }
-}
-
-const generateButton = $('button')
 
 generateButton.on('click', (e)=>{
-  // console.log('button pressed');
-  totalFacts()
+
+  makeRequest().then((response)=>{$numberFactsDiv.append(`<p>${response.data}</p>`)})
+    .then(()=>{
+      makeRequest().then((response)=>{$numberFactsDiv.append(`<p>${response.data}</p>`)})
+    })
+    .then(()=>{
+      makeRequest().then((response)=>{$numberFactsDiv.append(`<p>${response.data}</p>`)})
+    })
+    .then(()=>{
+      makeRequest().then((response)=>{$numberFactsDiv.append(`<p>${response.data}</p>`)})
+    })
+
 })
-
-
-// makeRequest.catch(
-//   err => {
-//   console.log(`Counldn't make the request, due to:`);
-//   console.log(err);
-//   console.log(`End`);
-// })
-
-const $numberFactsDiv = $('.number-facts')
