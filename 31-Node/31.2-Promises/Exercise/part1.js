c3ea2de018ca62ec257058428dc90bb75d26a68f
shivajreddy <shivajayaramireddy@gmail.com>
2022-04-27 07:12:11 -0400

    added axios to promises exercises

--- 31-Node/31.2-Promises/Exercise/part1.js
+++ 31-Node/31.2-Promises/Exercise/part1.js
@@ -1,7 +1,26 @@
+const axios = require('axios');
 const baseURL = 'http://numbersapi.com'
 const favNum = 2
 
-const makeRequest = $.get(`${baseURL}/${favNum}`)
+// const makeRequest = $.get(`${baseURL}/${favNum}`)
+
+// by making promise object
+function makeRequest(){
+  return new Promise((resolve, reject) => {
+    // const response = $.get(`${baseURL}/${favNum}`)
+    const reqponse = axios.get(`${baseURL}/${favNum}`)
+    console.log(response)
+    resolve(response)
+  })
+}
+
+makeRequest().then((response)=>{
+  console.log(`Successful request`)
+  console.log(response)
+})
+
+
+// by using axios to send get request
 
 const totalFacts = (times) => {
   for (let i=0; i<times; i++){
@@ -14,7 +33,6 @@ const totalFacts = (times) => {
 }
 
 const generateButton = $('button')
-console.log(generateButton);
 
 generateButton.on('click', (e)=>{
   // console.log('button pressed');
@@ -22,14 +40,11 @@ generateButton.on('click', (e)=>{
 })
 
 
-makeRequest.catch(
-  err => {
-  console.log(`Counldn't make the request, due to:`);
-  console.log(err);
-  console.log(`End`);
-})
+// makeRequest.catch(
+//   err => {
+//   console.log(`Counldn't make the request, due to:`);
+//   console.log(err);
+//   console.log(`End`);
+// })
 
 const $numberFactsDiv = $('.number-facts')
-
-
-// test
