c1e5083663239da5471f38bc3ad8a185ce7e7953
shivajreddy <shivajayaramireddy@gmail.com>
2022-05-03 18:10:29 -0400

    finished 33 broken app

--- 33-BrokenApp/P3-broken-app/app.js
+++ 33-BrokenApp/P3-broken-app/app.js
@@ -1,18 +1,51 @@
 const express = require('express');
 let axios = require('axios');
 var app = express();
+const ExpressError = require('./expresserror');
 
-app.post('/', function(req, res, next) {
+app.use(express.json());
+
+function make_git_request(dev_name){
+  const response = axios.get(`https://api.github.com/users/${dev_name}`);
+  return response
+}
+
+app.post('/p2', async function(req,res,next){
+  try{
+    if (!req.body.developers){throw new ExpressError('no developer object in body', 404)};
+
+    const devs = req.body.developers.map(name => make_git_request(name))
+    const result = [];
+    for await (const dev of devs){
+      result.push({name:dev.data.name, bio:dev.data.bio});
+    }
+    return res.send(JSON.stringify(result));
+  } catch(e){
+    next(e)
+  }
+})
+
+
+// old code fixed using Promise.all()
+app.post('/', async function(req, res, next) {
   try {
+    console.log(`mapping for these devs`, req.body.developers)
     let results = req.body.developers.map(async d => {
       return await axios.get(`https://api.github.com/users/${d}`);
     });
-    let out = results.map(r => ({ name: r.data.name, bio: r.data.bio }));
+    const resolved = await Promise.all(results);
+    let out = resolved.map((r) => ({ name: r.data.name, bio: r.data.bio }));
 
     return res.send(JSON.stringify(out));
-  } catch {
+  } catch (err) {
     next(err);
   }
 });
 
-app.listen(3000);
+// Global Error handler
+app.use((error, req, res, next)=>{
+  return res.status(error.status).send(error.msg)
+})
+
+
+app.listen(3000, ()=>{`App started at port 3000`});
--- 33-BrokenApp/P3-broken-app/issues.md
+++ 33-BrokenApp/P3-broken-app/issues.md
@@ -1,1 +1,13 @@
 # Broken App Issues
+
+### 1. Not catching the error that is being thrown.
+
+### 2. The request being sent to all the usernames is being mapped to 'results'
+### but 2nd mapper is not waiting until all the promises are resolved.
+
+### 3. changed the variable names from the following left to righ:
+#### result -> devs
+#### devs mapping item d -> name
+#### out -> result
+
+### 4. no global error handler. Used the custom ExpressError object to throw wrong inputs.
