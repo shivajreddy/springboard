71faf090a5682b00210c569929c22cdc7c95f365
shivajreddy <shivajayaramireddy@gmail.com>
2022-05-13 19:22:17 -0400

    added routes, but have to move jwt auth to middleware

--- 36-ExpressIntermediate/36.1-Hashing,WebTokens/Messagely/models/user.js
+++ 36-ExpressIntermediate/36.1-Hashing,WebTokens/Messagely/models/user.js
@@ -7,28 +7,33 @@ const ExpressError = require('../expressError');
 
 class User {
 
-  constructor(username, password, first_name, last_name, phone) {
+  constructor(username, password, first_name, last_name, phone, join_at, last_login_at) {
     this.username = username;
     this.password = password;
     this.first_name = first_name;
     this.last_name = last_name;
     this.phone = phone;
+    this.join_at = join_at;
+    this.last_login_at = last_login_at;
   }
 
   /** register new user -- returns
    *    {username, password, first_name, last_name, phone}
    */
 
-  static async register({ given_username, given_password, given_first_name, given_last_name, given_phone }) {
+  static async register({ username: given_username, hashed_pw: given_password, first_name: given_first_name, last_name: given_last_name, phone: given_phone }) {
+    const temp_join_at = new Date();
+    const temp_last_login = new Date();
     const result = await db.query(
-      `INSERT INTO users (username, password, first_name, last_name, phone)
-      VALUES ($1, $2, $3, $4, $5)`,
-      [given_username, given_password, given_first_name, given_last_name, given_phone]
+      `INSERT INTO users (username, password, first_name, last_name, phone, join_at, last_login_at)
+        VALUES ($1, $2, $3, $4, $5, $6, $7)
+        RETURNING * `,
+      // RETURNING username, password, first_name, last_name, phone`,
+      [given_username, given_password, given_first_name, given_last_name, given_phone, temp_join_at, temp_last_login]
     );
-    if (result.rowCount === 0) { throw new ExpressError('Couldnt create user', 404) };
-    const { username, password, first_name, last_name, phone } = result.rows[0]
-    const new_user = User(username, password, first_name, last_name, phone);
-    return new_user;
+    const psql_result = result.rows[0];
+    const new_usr = new User(psql_result.username, psql_result.password, psql_result.first_name, psql_result.last_name, psql_result.phone, psql_result.join_at, psql_result.last_login_at);
+    return new_usr;
   }
 
   /** Authenticate: is this username/password valid? Returns boolean. */
@@ -58,7 +63,17 @@ class User {
    *          join_at,
    *          last_login_at } */
 
-  static async get(username) { }
+  static async get(username) {
+    const result = await db.query(
+      `SELECT * FROM users
+      WHERE username=$1`
+      , [username]
+    )
+    if (result.rowCount === 0) throw new ExpressError(`No user of ${username}`, 403);
+    const psql_row = result.rows[0];
+    const usr = new User(psql_row.username, psql_row.password, psql_row.first_name, psql_row.last_name, psql_row.phone, psql_row.join_at, psql_row.last_login_at);
+    return usr;
+  }
 
   /** Return messages from this user.
    *
@@ -79,6 +94,17 @@ class User {
    */
 
   static async messagesTo(username) { }
+
+  async save() {
+    const result = await db.query(
+      `UPDATE users
+      SET password=$2, first_name=$3, last_name=$4, phone=$5, join_at=$6, last_login_at=$7
+      WHERE username=$1
+      RETURNING * `,
+      [this.username, this.password, this.first_name, this.last_name, this.phone, this.join_at, this.last_login_at]);
+
+    return result.rows[0];
+  }
 }
 
 
--- 36-ExpressIntermediate/36.1-Hashing,WebTokens/Messagely/routes/auth.js
+++ 36-ExpressIntermediate/36.1-Hashing,WebTokens/Messagely/routes/auth.js
@@ -1,10 +1,13 @@
 // authentication middleware
 const express = require('express');
+const ExpressError = require('../expressError');
 const authRoutes = express.Router();
 
-authRoutes.get('/', (req, res, next)=>{
-  return res.status(200).json({msg:"reached the auth routes hp"});
-})
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+
+const { SECRET_KEY, BCRYPT_WORK_FACTOR } = require('../config');
+const User = require('../models/user');
 
 
 /** POST /login - login: {username, password} => {token}
@@ -12,38 +15,90 @@ authRoutes.get('/', (req, res, next)=>{
  * Make sure to update their last-login!
  *
  **/
-authRoutes.post('/login', (req, res, next)=>{
+authRoutes.post('/login', async (req, res, next) => {
+  /*format object to login
+  {
+    "username" : "",
+    "password" : ""
+  }
+  */
+  try {
+    const { username, password } = req.body;
 
-  // bcrypt.compare()
+    if (!username || !password) throw new ExpressError(`no username or pass`, 404);
 
-  // jwt.sign()
+    const usr = await User.get(username);
+    console.log('this is the usr', usr);
+
+    usr.last_login_at = new Date();
+    usr.save();
+    console.log('this is the usr after saving', usr);
+
+    // compare with bcrypt.compare()
+    const correct_password = await bcrypt.compare(password, usr.password);
+
+    // sign with jwt if correct password
+    if (correct_password) {
+      const pay_load = usr;
+      const token = jwt.sign({ pay_load }, SECRET_KEY);
+      req.body._token = token;
+      req.user = pay_load;
+    } else {
+      return res.status(403).json({ result: `password incorrect for ${username}` });
+    }
+    return next();
+
+  } catch (error) {
+    // console.log('error handling for login route')
+    return next(error);
+  }
 
 })
 
 // call back fn for app
-
+// TODO
 /** POST /register - register user: registers, logs in, and returns token.
  *
  * {username, password, first_name, last_name, phone} => {token}.
  *
  *  Make sure to update their last-login!
  */
-authRoutes.post('/register', async(req, res, next)=>{
+authRoutes.post('/register', async (req, res, next) => {
   /*  Format of the body to create a new user
   {
-    
+    username: "not existing in db",
+    password: "not null",
+    first_name: "not null",
+    last_name: "not nul",
+    phone: "text not null",
+    -> join_at : auto gen. date,
+    -> last_login : empty
   }
   */
   try {
     // get the user information
-    const {} = req.body;
+    const { username, password, first_name, last_name, phone } = req.body;
+
+    // error checks
+    if (!username || !password || !first_name || !last_name || !phone) throw new ExpressError(`given usr object has formatting errors`, 404);
+
     // bcrypt.hash()
+    const hashed_pw = await bcrypt.hash(password, BCRYPT_WORK_FACTOR);
 
-    // save the hashed password to bcrypt
+    // create user object with given details + hashed_pw 
+    const usr = { username, hashed_pw, first_name, last_name, phone };
 
-    // 
+    // save the user details to db
+    const new_user = await User.register(usr);
+
+    return res.status(201).json({ new_user })
+
   } catch (error) {
-    return next(error);
+    if (error.code === '23505') {
+      return next(new ExpressError(`username:${req.body.username} already taken`, 404));
+    } else {
+      return next(error);
+    }
   }
 })
 
