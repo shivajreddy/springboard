3dd6924243cf572d2af137e5207a222a2c7b50b6
Shiva <shiva@Englands-MacBook-Pro.local>
2022-05-19 19:09:57 -0400

    39.3 finished

--- 39-React/39.3-CRA/exercise/src/index.js
+++ 39-React/39.3-CRA/exercise/src/index.js
@@ -3,10 +3,19 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
+import foods from './foods'
+import { choice, remove } from './helper'
+
+
+console.log('all items', foods);
+const random_food = choice(foods);
+console.log("Heres your random fruit", random_food);
+remove(foods, random_food);
+
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
-
