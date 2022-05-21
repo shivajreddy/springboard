ccd2680c27de3edf1b5f414880ee0aeba7b75f49
Shiva <shiva@Englands-MacBook-Pro.local>
2022-05-21 13:13:54 -0400

    eight ball, 2 states for msg and color, global object to track color count

--- 39-React/39.4-Events&State/exercise/src/EightBall.js
+++ 39-React/39.4-Events&State/exercise/src/EightBall.js
@@ -1,22 +1,50 @@
 import React, { useState } from 'react'
 import msgs from './messages'
+import './EightBall.css'
+
+const color_count = {
+  goldenrod: 0,
+  red: 0,
+  green: 0
+}
 
 function EightBall(props) {
+  const default_msg = "Ask me anything human 🧞‍♂️"
 
-  const [curr_state, setState] = useState();
-  console.log(curr_state)
+  const [curr_msg, setMsg] = useState(default_msg);
+  const [curr_color, setColor] = useState();
 
-  function ball_answer() {
-    const random_obj = msgs[(Math.floor(Math.random() * (msgs.length)) + 1)];
-    return random_obj.msg
+  function ask_ball() {
+    const random_obj = msgs[(Math.floor(Math.random() * (msgs.length)))];
+    setMsg(random_obj.msg)
+    setColor(random_obj.color)
+    if (random_obj.color === 'red') color_count.red += 1;
+    if (random_obj.color === 'green') color_count.green += 1;
+    if (random_obj.color === 'goldenrod') color_count.goldenrod += 1;
+  }
+
+  function reset_game() {
+    setMsg(default_msg);
+    setColor();
   }
 
   return (
-    <>
-      EightBall
-      <p>{curr_state}</p>
-      <button onClick={() => setState(ball_answer())}>push</button>
-    </>
+    <div className='EightBall-game'>
+      <div className='EightBall-ball' style={{ backgroundColor: curr_color }}>
+        <h3 style={{ padding: "10px" }}>{curr_msg}</h3>
+        {/* <h3>{curr_color}</h3> */}
+      </div>
+      <button onClick={ask_ball} className="EightBall-btn">Ask Ball</button>
+      {curr_color
+        &&
+        <button onClick={reset_game}>Reset</button>
+      }
+      {
+        curr_color
+        &&
+        <p>color count- Red:{color_count.red} Green:{color_count.green} GoldenRod:{color_count.goldenrod}</p>
+      }
+    </div>
 
   )
 }
