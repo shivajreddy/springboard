084cb7bfcccdece0d24958797b7638ccb3e14a00
Shiva <shiva@Englands-MacBook-Pro.local>
2022-05-23 11:57:14 -0400

    delete button deletes that list item using state

--- 39-React/39.6-State Patterns/followalong/src/App.js
+++ 39-React/39.6-State Patterns/followalong/src/App.js
@@ -1,9 +1,27 @@
 import './App.css';
+import Counter from './Counter';
+
+import RemoveNumber from './RemoveNumber';
+
+import NumberList from './NumberList';
+const nums = [1,2,3,4,5,6,7,8,9,10,11,12,13,14];
+
 
 function App() {
   return (
     <>
-      <h1>React App</h1>
+      {/* <h1>React</h1> */}
+      {/* <Counter/>
+
+      <NumberList
+        all_numbers = {nums}
+        remove_number={10}
+      /> */}
+
+      <RemoveNumber
+        all_numbers = {nums}
+      /> 
+      
     </>
   );
 }
---
+++ 39-React/39.6-State Patterns/followalong/src/RemoveNumber.js
@@ -0,0 +1,29 @@
+import React, {useState} from 'react'
+
+function RemoveNumber(props) {
+
+  const [all_numbers, setAllNumbers] = useState(props.all_numbers);
+
+  function delete_button(num) {
+    console.log('dele tb', num)
+    setAllNumbers(props.all_numbers.filter(item=>item!==num))
+  }
+
+  const all_buttons = all_numbers.map((num,idx)=>{
+    return(
+      <li key={idx}>{num} <button onClick={()=>delete_button(num)}>-</button></li>
+
+    )
+  })
+  
+
+  return (
+    <>
+      <ul>
+        {all_buttons}
+      </ul>
+    </>
+  )
+}
+
+export default RemoveNumber
