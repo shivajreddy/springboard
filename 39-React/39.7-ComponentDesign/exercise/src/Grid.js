dcb3e3ce2be82a970aab4267cc4758d0f8f43afa
SPW <shivajreddy@outlook.com>
2022-07-12 09:28:55 -0400

    .

--- 39-React/39.7-ComponentDesign/exercise/src/Grid.js
+++ 39-React/39.7-ComponentDesign/exercise/src/Grid.js
@@ -3,19 +3,37 @@ import './Grid.js'
 import './Grid.css'
 import Light from './Light.js'
 
+function creteMatrix(r, c) {
 
-function creteMatrix(r, c){
-  const matrix = Array(r).fill(1).map(()=>Array(c).fill(1))
+  const matrix = Array(r).fill(<h1>1</h1>).map(() => Array(c).fill(<h1>1</h1>))
+
+  return matrix
 }
 
+// const matrix = Array(r).fill(1).map(() => Array(c).fill(1))
 
-function Grid({width, height}) {
+const func21 = () => {
+  // const matrix = Array(9).fill().map(() => Array(3).fill())
+  // const gameDiv = <div></div>;
+  // for (row in matrix) {
+  //   for (col in row) {
+  //     <h1>1</h1>
+  //   }
+  // }
+  Array(3).fill().map(() => Array(3).fill().map(() => 1))
+}
 
-
+function Grid({ width, height }) {
 
   return (
     <div className='Grid'>
       <h2>Grid: {width} x {height} </h2>
+
+      <div>
+      {
+        Array(3).fill(1).map(() => new Array(3).fill(1))
+      }
+      </div>
 
       {/* <Light r={} c={}/> */}
 
--- 39-React/39.7-ComponentDesign/exercise/src/Light.js
+++ 39-React/39.7-ComponentDesign/exercise/src/Light.js
@@ -2,11 +2,11 @@ import React from 'react'
 import './Light.css'
 
 
-
 function Light() {
   return (
     <div>Light</div>
   )
 }
+
 
 export default Light
