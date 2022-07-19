c46690b93ddf8de988d94dd1d4423e109afee334
shivajreddy <shivajayaramireddy@gmail.com>
2022-07-19 18:05:52 -0400

    followalong pracise router links for router pattern

--- 41-Router/41.2-RouterPatterns/followalong/src/App.jsx
+++ 41-Router/41.2-RouterPatterns/followalong/src/App.jsx
@@ -4,6 +4,7 @@ import Nav from "components/Nav";
 import HomePage from "pages/HomePage";
 import Page1 from "pages/Page1";
 import Page2 from "pages/Page2";
+import Error404 from "pages/Error404";
 
 function App() {
   return (
@@ -14,6 +15,12 @@ function App() {
         <Route path="/homepage" element={<HomePage />}></Route>
         <Route path="/page1" element={<Page1 />} />
         <Route path="/page2" element={<Page2 />} />
+        <Route path="product">
+          <Route path="1" element={<HomePage />} />} />
+          <Route path="2" element={<Page1 />} />
+          <Route path="2" element={<Page2 />} />
+        </Route>
+        <Route path="*" element={<Error404 />} />
       </Routes>
     </BrowserRouter>
   );
