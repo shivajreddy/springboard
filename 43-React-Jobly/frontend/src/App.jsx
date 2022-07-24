d9e329b7fd71687502f85e0cf6768afab622bba1
shivajreddy <shivajayaramireddy@gmail.com>
2022-07-24 17:32:02 -0400

    Jobly starter code

---
+++ 43-React-Jobly/frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import AppRoutes from "AppRoutes";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Jobly</h1>
+      <AppRoutes />
+    </div>
+  );
+}
+
+export default App;
