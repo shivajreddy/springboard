d28dbb82ce07961f6ac248e8197bfa6c5aad6af5
Shiva TEClab <shivajreddy@outlook.com>
2022-07-25 16:25:16 -0400

    test express practise

--- 37-express-jobly/TestExpress/server.js
+++ 37-express-jobly/TestExpress/server.js
@@ -7,6 +7,14 @@ app.get("/", (req, res) => {
   res.send("Hello World");
 });
 
+// URL parameters
+app.get("/url/:color", (req, res) => {
+  return res.send(req.params.color);
+});
+
+// Headers
+app.get("/headers", (req, res) => {});
+
 app.listen(PORT, () => {
   console.log("App started listening at ", PORT);
 });
