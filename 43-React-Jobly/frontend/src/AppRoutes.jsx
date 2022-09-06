ba16ce806094d5433a96bb1a167d42b941e6a55c
Shiva TEClab <shivajreddy@outlook.com>
2022-09-06 15:56:16 -0400

    frontend react router, components basic layout

--- 43-React-Jobly/frontend/src/AppRoutes.jsx
+++ 43-React-Jobly/frontend/src/AppRoutes.jsx
@@ -1,14 +1,10 @@
 import React from "react";
 import NavBarMain from "components/NavBar";
-import Test from "components/Test";
-import { Link } from "react-router-dom";
 
 function AppRoutes() {
   return (
     <div>
       <NavBarMain />
-      <h1>App routes</h1>
-      <Test />
     </div>
   );
 }
--- 43-React-Jobly/frontend/src/components/NavBar.jsx
+++ 43-React-Jobly/frontend/src/components/NavBar.jsx
@@ -1,30 +1,39 @@
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
-import { BrowserRouter, Routes, Route, Link, NavLink } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import Page1 from "pages/Page1";
 import Page2 from "pages/Page2";
 import Page3 from "pages/Page3";
 
 function NavBarMain() {
+  const linkStyle = {
+    color: "black",
+    paddingLeft: "10px",
+    textDecoration: "none",
+  };
   return (
     <BrowserRouter>
       <Navbar bg="primary" variant="dark">
         <Container>
-          <Navbar.Brand href="#home">Navbar</Navbar.Brand>
-          <Nav className="me-auto">
-            <Link to="/page-1" style={{ color: "black" }}>
+          <Navbar.Brand>
+            <Link to="/" style={linkStyle}>
               Page1
             </Link>
-            <Link to="/page-2" style={{ color: "black" }}>
+          </Navbar.Brand>
+          <Nav className="me-auto">
+            <Link to="/page-1" style={linkStyle}>
+              Page1
+            </Link>
+            <Link to="/page-2" style={linkStyle}>
               Page2
             </Link>
-            <Link to="/page-3" style={{ color: "black" }}>
+            <Link to="/page-3" style={linkStyle}>
               Page3
             </Link>
-            <Nav.Link>Page1</Nav.Link>
+            {/* <Nav.Link>Page1</Nav.Link>
             <Nav.Link href="/page-2">Page2</Nav.Link>
-            <Nav.Link href="/page-3">Page3</Nav.Link>
+            <Nav.Link href="/page-3">Page3</Nav.Link> */}
           </Nav>
         </Container>
       </Navbar>
