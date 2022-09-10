08b3ad334545e6fe9a035f1a1496a815c580d331
ShivaSMBP <shivajayaramireddy@gmail.com>
2022-09-10 16:44:59 -0400

    render company card after axios call to /companies

--- 43-React-Jobly/frontend/src/components/cards/Company.tsx
+++ 43-React-Jobly/frontend/src/components/cards/Company.tsx
@@ -23,8 +23,8 @@ export function Company(props: Details) {
       <Card
         sx={{
           borderRadius: 4,
+          width: 900,
           height: 318,
-          maxWidth: 279,
           margin: "10px",
           textAlign: "start",
           transitionDuration: "0.1s",
@@ -55,29 +55,36 @@ export function Company(props: Details) {
             position: "absolute",
             overflow: "visible",
             top: "59%",
-            left: "11%",
             transform: "translateX(-50%)",
           }}
         >
-          <Typography variant="h5" fontWeight={700} color="custom1">
-            {props.compDetails.name}
-          </Typography>
-          {props.compDetails.num_employees && (
-            <div>
-              <PeopleAltIcon fontSize="small" sx={{ paddingTop: "4px" }} />
-              <Typography
-                sx={{ mb: 1.5 }}
-                color="text.primary"
-                display="flex"
-                alignContent="center"
-              >
-                {props.compDetails.num_employees}
-              </Typography>
-            </div>
-          )}
-          <Typography sx={{ mb: 1.5 }} color="text.secondary">
-            About: {props.compDetails.description}
-          </Typography>
+          <Box
+            sx={{
+              position: "absolute",
+              left: "50%",
+              width: "100%",
+            }}
+          >
+            <Typography variant="h5" fontWeight={700} color="custom1">
+              {props.compDetails.name}
+            </Typography>
+            {props.compDetails.num_employees && (
+              <div>
+                <PeopleAltIcon fontSize="small" sx={{ paddingTop: "4px" }} />
+                <Typography
+                  sx={{ mb: 1.5 }}
+                  color="text.primary"
+                  display="flex"
+                  alignContent="center"
+                >
+                  {props.compDetails.num_employees}
+                </Typography>
+              </div>
+            )}
+            <Typography sx={{ mb: 1.5 }} color="text.secondary">
+              About: {props.compDetails.description}
+            </Typography>
+          </Box>
         </CardContent>
       </Card>
     </Box>
--- 43-React-Jobly/frontend/src/pages/Companies.tsx
+++ 43-React-Jobly/frontend/src/pages/Companies.tsx
@@ -3,6 +3,9 @@ import React from "react";
 import { Company } from "../components/cards/Company";
 import axios from "axios";
 import config from "../config.json";
+import { Box } from "@mui/system";
+import Loading from "../components/Loading";
+import { Typography } from "@mui/material";
 
 const sampleData = {
   handle: "test-handle",
@@ -12,26 +15,51 @@ const sampleData = {
   logo_url: null,
 };
 
+interface CompanyInterface {
+  name: string;
+  handle: string;
+  num_employees: string | null;
+  description: string;
+  logo_url: string;
+}
+
 function Companies() {
-  const [companies, setCompanies] = React.useState();
-  console.log(config);
+  const [companies, setCompanies] = React.useState<CompanyInterface[]>();
 
   React.useEffect(() => {
     async function makeRequest() {
       const res = await axios.get(config.BASE_URL + "/companies");
-      console.log(res);
-      return res.data;
+      setCompanies(res.data.companies);
+      return res.data.companies;
     }
     makeRequest();
   }, []);
 
   return (
-    <>
-      <Company compDetails={sampleData} />
-      <Company compDetails={sampleData} />
-      <Company compDetails={sampleData} />
-      <Company compDetails={sampleData} />
-    </>
+    <div>
+      <Typography variant="h3" textAlign="start">
+        Companies
+      </Typography>
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          alignContent: "center",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        {!companies && <Loading />}
+        {companies &&
+          companies.map((c: CompanyInterface) => (
+            <Company key={c.handle} compDetails={c} />
+          ))}
+        <Company compDetails={sampleData} />
+        <Company compDetails={sampleData} />
+        <Company compDetails={sampleData} />
+        <Company compDetails={sampleData} />
+      </Box>
+    </div>
   );
 }
 
