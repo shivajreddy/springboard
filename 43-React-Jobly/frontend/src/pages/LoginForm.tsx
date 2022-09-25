d3ff6340846c19ae20c051aa451ea35652e4a37c
ShivaSMBP <shivajayaramireddy@gmail.com>
2022-09-25 12:00:32 -0400

    member modifiers

--- 43-React-Jobly/frontend/src/pages/LoginForm.tsx
+++ 43-React-Jobly/frontend/src/pages/LoginForm.tsx
@@ -3,73 +3,114 @@ import TextField from "@mui/material/TextField";
 import AssignmentIndIcon from "@mui/icons-material/AssignmentInd";
 import PasswordIcon from "@mui/icons-material/Password";
 import { Button, Typography } from "@mui/material";
-import axios from "axios";
 import config from "../config.json";
 import useLocalStorage from "../hooks/useLocalStorage";
+import JoblyApi from "../utilities/joblyAPI";
+import { IUser } from "./Profile";
+import { useState } from "react";
 
 export default function LoginForm() {
   const [token, setToken] = useLocalStorage("token");
-  /** Login */
-  function login(e: React.FormEvent<HTMLFormElement>) {
+  const [statusMessage, setStatusMessage] = useState([]);
+  const [formErrors, setFormErrors] = useState<any>([]);
+  const [formData, setFormData] = useState({
+    username: "",
+    password: "",
+  });
+
+  async function login(data: Pick<IUser, "username" | "password">) {
+    try {
+      let token = await JoblyApi.login(data);
+      setToken(token);
+      return { success: true };
+    } catch (error) {
+      console.error("login failed", error);
+      return { success: false, error };
+    }
+  }
+
+  /** Handle form submission */
+  async function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
     e.preventDefault();
     const username = (
-      e.currentTarget.elements.namedItem("form-username") as HTMLInputElement
+      e.currentTarget.elements.namedItem("username") as HTMLInputElement
     ).value;
     const password = (
-      e.currentTarget.elements.namedItem("form-password") as HTMLInputElement
+      e.currentTarget.elements.namedItem("password") as HTMLInputElement
     ).value;
-    async function makeRequest() {
-      const res = await axios.post(config.BASE_URL + "/auth/token", {
-        username: username,
-        password: password,
-      });
-      setToken(res.data.token);
+    console.log(username, password);
+    // let result = await login()
+    const result = await login(formData);
+    if (result.success) {
+    } else {
+      setFormErrors(result.error);
     }
-    makeRequest();
-    return 21;
+    console.log("this is the result on line 43 in loginform component", result);
+  }
+  console.log("these are the errors", formErrors);
+
+  /** Handle form change events */
+  function handleChange(e: any) {
+    // e.preventDefault();
+    const name = e.target.name;
+    const value = e.target.value;
+    setFormData((l) => ({ ...l, [name]: value }));
   }
 
   // TODO redirect to user page if already logged in
-  console.log("token found in login form", token, typeof token);
+  // console.log("token found in login form", token, typeof token);
   if (token === "null" || token === null) {
     return (
-      <Box
-        component="form"
-        sx={{
-          display: "flex",
-          flexDirection: "column",
-          marginTop: "50px",
-          alignItems: "center",
-        }}
-        autoComplete="off"
-        onSubmit={login}
-      >
-        <Typography variant="h6" fontWeight={700}>
-          Login
-        </Typography>
-        <div style={{ margin: "10px" }}>
-          <AssignmentIndIcon sx={{ margin: "20px" }} />
-          <TextField name="form-username" label="Username" variant="standard" />
-        </div>
-        <div style={{ margin: "10px" }}>
-          <PasswordIcon sx={{ margin: "20px" }} />
-          <TextField
-            name="form-password"
-            label="Password"
-            type="password"
-            autoComplete="current-password"
-            variant="standard"
-          />
-        </div>
-        <Button
-          color="primary"
-          variant="contained"
-          sx={{ alignContent: "center" }}
-          type="submit"
+      <>
+        <Box
+          component="form"
+          sx={{
+            display: "flex",
+            flexDirection: "column",
+            marginTop: "50px",
+            alignItems: "center",
+          }}
+          autoComplete="off"
+          onSubmit={handleSubmit}
         >
-          Log in
-        </Button>
-      </Box>
+          <Typography variant="h6" fontWeight={700}>
+            Login
+          </Typography>
+          <div style={{ margin: "10px" }}>
+            <AssignmentIndIcon sx={{ margin: "20px" }} />
+            <TextField
+              name="username"
+              label="Username"
+              variant="standard"
+              value={formData.username}
+              onChange={handleChange}
+            />
+          </div>
+          <div style={{ margin: "10px" }}>
+            <PasswordIcon sx={{ margin: "20px" }} />
+            <TextField
+              name="password"
+              label="Password"
+              type="password"
+              autoComplete="current-password"
+              variant="standard"
+              value={formData.password}
+              onChange={handleChange}
+            />
+          </div>
+          <Button
+            color="primary"
+            variant="contained"
+            sx={{ alignContent: "center" }}
+            type="submit"
+          >
+            Log in
+          </Button>
+          {formErrors.length ? (
+            <p className="text-red-800">{formErrors}</p>
+          ) : null}
+        </Box>
+      </>
     );
   } else {
     return <p>Already logged in</p>;
--- 43-React-Jobly/frontend/src/utilities/joblyAPI.ts
+++ 43-React-Jobly/frontend/src/utilities/joblyAPI.ts
@@ -69,9 +69,9 @@ class JoblyApi {
     return res.jobs;
   }
 
-  /** Login with username and passwrod */
+  /** Login with username and password */
   static async login(data: Pick<IUser, "username" | "password">) {
-    console.log("making log in with this data", data);
+    console.log("making joblyAPI line74 log in with this data", data);
     let res = await this.request(`auth/token`, data, "post");
     return res.token;
   }
